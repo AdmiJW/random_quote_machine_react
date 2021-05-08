a27a8c4677e200e4c97b243dfb51a385823d4dbb
AdmiJW <jamweg22@gmail.com>
2021-05-08 13:50:04 +0800

    Intiial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MainDiv from './Components/MainDiv';
+
+import './Styles/style.css';
+
+class App extends React.Component {
+  render() {
+    return (
+        <main>
+          <MainDiv />
+        </main>
+    );
+  }
+}
+
+export default App;
