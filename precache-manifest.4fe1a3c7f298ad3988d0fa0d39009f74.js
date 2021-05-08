1c6e85c8349ee20d22cb126b22610a5f7447fec3
AdmiJW <jamweg22@gmail.com>
2021-05-08 13:56:37 +0800

    Updates

---
+++ precache-manifest.4fe1a3c7f298ad3988d0fa0d39009f74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d2fd241db46ef1acfba8b2a1d61548e",
+    "url": "/random_quote_machine_react/index.html"
+  },
+  {
+    "revision": "708314fde3ca59ab5f5c",
+    "url": "/random_quote_machine_react/static/css/main.fe78f32c.chunk.css"
+  },
+  {
+    "revision": "4890d09f0e301b5e4ff1",
+    "url": "/random_quote_machine_react/static/js/2.addc0c12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random_quote_machine_react/static/js/2.addc0c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "708314fde3ca59ab5f5c",
+    "url": "/random_quote_machine_react/static/js/main.d3194c97.chunk.js"
+  },
+  {
+    "revision": "7872793052d32c749a01",
+    "url": "/random_quote_machine_react/static/js/runtime-main.ca09f2b0.js"
+  }
+]);
